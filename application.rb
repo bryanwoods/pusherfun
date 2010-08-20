54798c6f874c054fdf6dde84ceededc7185b8039
Bryan Woods <bryanwoods4e@gmail.com>
2010-08-20 14:38:30 -0400

    Updates

--- application.rb
+++ application.rb
@@ -1,9 +1,13 @@
 require 'rubygems'
 require 'pusher'
+require 'mongo_mapper'
 require 'haml'
 require 'sinatra'
 
 configure do
+  MongoMapper.connection = Mongo::Connection.new(ENV['MONGOHQ_URL'])
+  MongoMapper.database = "pusher_fun"
+
   Pusher.app_id = '1786'
   Pusher.key = '09435da909450e9b4b6e'
   Pusher.secret = '6112f12f27007eaab27d'
@@ -13,11 +17,21 @@ configure do
   SECRET = Pusher.secret
 end
 
+class Message
+  include MongoMapper::Document
+  key :screen_name, String, :required => true
+  key :message, String, :required => true
+end
+
 get '/' do
+  @messages = Message.all
   haml(:index)
 end
 
 post '/messages/?' do
+  @screen_name = params[:screen_name]
   @message = params[:message]
-  Pusher['test_channel'].trigger('my_event', @message)
+  Pusher['test_channel'].trigger('my_event', "#{@screen_name} said: #{@message}")
+  message = Message.create({:screen_name => @screen_name, :message => @message})
+  message.save
 end
